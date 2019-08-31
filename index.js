0d41284624238012d81854dd223704879efd5a25
Joao Pedro <jp052@hotmail.com>
2019-08-31 14:03:59 -0300

    begin of the project

---
+++ index.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+async function pingLink () {
+	const urls = [params.url];
+	requests = url.map((url) => {
+		return new Promise((resolve, reject) => {
+			const option = {
+				url: url,
+				method: 'GET',
+				json: true
+			};
+			request(option, (err, resp) => {
+				if (err) reject(err);
+				else if (resp.statusCode == 200) resolve({msg: 'It\'s alive!'});
+				else reject({msg: 'It\'s not alive :('});
+			});
+		});
+	});
+	pings = Promise.all(requests);
+	const ret = pings.filter(data => data.msg == 'It\'s alive!');
+	return ret;
+}
+
+module.exports = pingLink;
